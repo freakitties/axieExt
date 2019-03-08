938a8cee1c270954789c49243dd7bded8c5fb0b4
freak <freakitties@gmail.com>
2019-03-07 22:14:51 -0800

    added hidden genes display

--- contentScript.js
+++ contentScript.js
@@ -9,13 +9,25 @@ var observer;
 const observerConfig = { attributes: false, childList: true, subtree: true };
 var currentURL = window.location.href;
 var axies = {};
-function init() {
+
+var colorMap = {
+    "plant": "rgb(108, 192, 0)",
+    "reptile": "rgb(200, 138, 224)",
+    "beast": "rgb(255, 184, 18)",
+    "aquatic": "rgb(0, 184, 206)",
+    "bird": "rgb(255, 139, 189)",
+    "bug": "rgb(255, 83, 65)"
+}
+
+async function init() {
     if (window.Web3) {
         //console.log("found web3");
         web3query = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/mew'));
         expCheckpointContract = web3query.eth.contract(expCheckpointABI);
         expCheckpointInstance = expCheckpointContract.at(expCheckpointAddress);
     }
+
+    await getBodyParts();
 
     let callback = function(mutationsList, observer) {
 //console.log("list", mutationsList);
@@ -54,6 +66,167 @@ function init() {
 
     };
     observer = new MutationObserver(callback);
+}
+
+var bodyPartsMap = {};
+async function getBodyParts() {
+    let parts = await fetch('https://axieinfinity.com/api/body-parts').
+        then(res => res.json()).
+        catch(async (err) => {
+            console.log("Failed to get body parts from the API");
+            //API is unreliable. fall back to hard-coded local copy.
+            let parts = await fetch(chrome.extension.getURL('body-parts.json')).then(res => res.json());
+            for (let i in parts) {
+                bodyPartsMap[parts[i].id] = parts[i];
+            }
+        });
+    for (let i in parts) {
+        bodyPartsMap[parts[i].id] = parts[i];
+    }
+}
+
+var classGeneMap = {"0000": "beast", "0001": "bug", "0010": "bird", "0011": "plant", "0100": "aquatic", "0101": "reptile", "1000": "???", "1001": "???", "1010": "???"};
+var typeOrder = {"patternColor": 1, "eyes": 2, "mouth": 3, "ears": 4, "horn": 5, "back": 6, "tail": 7};
+var geneColorMap = {"0000": {"0010": "ffec51","0011": "ffa12a","0100": "f0c66e", "0110": "60afce"},
+"0001": {"0010": "ff7183", "0011": "ff6d61", "0100": "f74e4e",},
+"0010": {"0010": "ff9ab8", "0011": "ffb4bb","0100": "ff778e"},
+"0011": {"0010": "ccef5e", "0011": "efd636","0100": "c5ffd9"},
+"0100": {"0010": "4cffdf", "0011": "2de8f2","0100": "759edb", "0110": "ff5a71"},
+"0101": {"0010": "fdbcff", "0011": "ef93ff","0100": "f5e1ff", "0110": "43e27d"},
+//nut hidden_1
+"1000": {"0010": "D9D9D9", "0011": "D9D9D9","0100": "D9D9D9", "0110": "D9D9D9"},
+//star hidden_2
+"1001": {"0010": "D9D9D9", "0011": "D9D9D9","0100": "D9D9D9", "0110": "D9D9D9"},
+//moon hidden_3
+"1010": {"0010": "D9D9D9", "0011": "D9D9D9","0100": "D9D9D9", "0110": "D9D9D9"}};
+const PROBABILITIES = {d: 0.375, r1: 0.09375, r2: 0.03125};
+const parts = ["eyes", "mouth" ,"ears", "horn", "back", "tail"];
+const MAX_QUALITY = 6 * (PROBABILITIES.d + PROBABILITIES.r1 + PROBABILITIES.r2);
+function getQualityAndPureness(traits, cls) {
+    let quality = 0;
+    let dPureness = 0;
+    for (let i in parts) {
+        if (traits[parts[i]].d.class == cls) {
+            quality += PROBABILITIES.d;
+            dPureness++;
+        }
+        if (traits[parts[i]].r1.class == cls) {
+            quality += PROBABILITIES.r1;
+        }
+        if (traits[parts[i]].r2.class == cls) {
+            quality += PROBABILITIES.r2;
+        }
+    }
+    return {quality: quality/MAX_QUALITY, pureness: dPureness};
+}
+
+function strMul(str, num) {
+    var s = "";
+    for (var i = 0; i < num; i++) {
+        s += str;
+    }
+    return s;
+}
+
+function genesToBin(genes) {
+    var genesString = genes.toString(2);
+    genesString = strMul("0", 256 - genesString.length) + genesString
+    return genesString;
+}
+
+const regionGeneMap = {"00000": "global", "00001": "japan"};
+function getRegionFromGroup(group) {
+    let regionBin = group.slice(8,13);
+    if (regionBin in regionGeneMap) {
+        return regionGeneMap[regionBin];
+    }
+    return "Unknown Region";
+}
+
+function getClassFromGroup(group) {
+    let bin = group.slice(0, 4);
+    if (!(bin in classGeneMap)) {
+        return "Unknown Class";
+    }
+    return classGeneMap[bin];
+}
+
+function getPatternsFromGroup(group) {
+    //patterns could be 6 bits. use 4 for now
+    return {d: group.slice(2, 8), r1: group.slice(8, 14), r2: group.slice(14, 20)};
+}
+
+function getColor(bin, cls) {
+    let color;
+    if (bin == "0000") {
+        color = "ffffff";
+    } else if (bin == "0001") {
+        color = "7a6767";
+    } else {
+        color = geneColorMap[cls][bin];
+    }
+    return color;
+}
+
+function getColorsFromGroup(group, cls) {
+    return {d: getColor(group.slice(20, 24), cls), r1: getColor(group.slice(24, 28), cls), r2: getColor(group.slice(28, 32), cls)};
+}
+
+//hack. key: part name + " " + part type
+var partsClassMap = {};
+function getPartName(cls, part, region, binary) {
+    let trait;
+    if (binary in binarytraits[cls][part]) {
+        if (region in binarytraits[cls][part][binary]) {
+            trait = binarytraits[cls][part][binary][region];
+        } else if ("global" in binarytraits[cls][part][binary]) {
+            trait = binarytraits[cls][part][binary]["global"];      //don't use mystic name. breeding doesn't pass mystic.
+        } else {
+            trait = "UNKNOWN Regional " + cls + " " + part;
+        }
+    } else {
+        trait = "UNKNOWN " + cls + " " + part;
+    }
+    //return part + "-" + trait.toLowerCase().replace(/\s/g, "-");
+    partsClassMap[trait + " " + part] = cls;
+    return trait;
+}
+
+function getPartsFromGroup(part, group, region) {
+    let mystic = group.slice(0, 2) == "11";
+    let dClass = classGeneMap[group.slice(2, 6)];
+    let dBin = group.slice(6, 12);
+    let dName = getPartName(dClass, part, region, dBin);
+
+    let r1Class = classGeneMap[group.slice(12, 16)];
+    let r1Bin = group.slice(16, 22);
+    let r1Name = getPartName(r1Class, part, region, r1Bin);
+
+    let r2Class = classGeneMap[group.slice(22, 26)];
+    let r2Bin = group.slice(26, 32);
+    let r2Name = getPartName(r2Class, part, region, r2Bin);
+
+    return {d: getPartFromName(part, dName), r1: getPartFromName(part, r1Name), r2: getPartFromName(part, r2Name), mystic: mystic};
+}
+
+function getTraits(genes) {
+    var groups = [genes.slice(0, 32), genes.slice(32, 64), genes.slice(64, 96), genes.slice(96, 128), genes.slice(128, 160), genes.slice(160, 192), genes.slice(192, 224), genes.slice(224, 256)];
+    let cls = getClassFromGroup(groups[0]);
+    let region = getRegionFromGroup(groups[0]);
+    let pattern = getPatternsFromGroup(groups[1]);
+    let color = getColorsFromGroup(groups[1], groups[0].slice(0, 4));
+    let eyes = getPartsFromGroup("eyes", groups[2], region);
+    let mouth = getPartsFromGroup("mouth", groups[3], region);
+    let ears = getPartsFromGroup("ears", groups[4], region);
+    let horn = getPartsFromGroup("horn", groups[5], region);
+    let back = getPartsFromGroup("back", groups[6], region);
+    let tail = getPartsFromGroup("tail", groups[7], region);
+    return {cls: cls, region: region, pattern: pattern, color: color, eyes: eyes, mouth: mouth, ears: ears, horn: horn, back: back, tail: tail};
+}
+
+function getPartFromName(traitType, partName) {
+    let traitId = traitType.toLowerCase() + "-" + partName.toLowerCase().replace(/\s/g, "-").replace(/[\?']/g, "");
+    return bodyPartsMap[traitId];
 }
 
 function checkStatus(res) {
@@ -96,8 +269,44 @@ async function getAxieInfo(id) {
         //var result_json = await fetch('https://axieinfinity.com/api/axies/' + id).then(res => res.json());
         var result_json = await fetch('https://api.axieinfinity.com/v1/axies/' + id).then(res => res.json());
         axies[id] = result_json;
-        return result_json;
+
+        axies[id].genes = genesToBin(BigInt(axies[id].genes));
+        let traits = getTraits(axies[id].genes);
+        let qp = getQualityAndPureness(traits, axies[id].class);
+        axies[id].traits = traits;
+        axies[id].quality = qp.quality;
+        axies[id].pureness = qp.pureness;
+        return axies[id];
     }
+}
+
+function appendTrait(table, trait) {
+    let row = document.createElement("tr");
+    let positions = [];
+    for (let position in trait) {
+        if (position == "mystic") continue;
+        let data = document.createElement("td");
+        let span = document.createElement("span");
+        if (trait[position].hasOwnProperty("class")) {
+            span.style.color = colorMap[trait[position].class];
+        }
+        span.textContent = trait[position].name;
+        data.appendChild(span);
+        row.appendChild(data);
+        //positions.push(span);
+    }
+    /*
+    let comma = document.createElement("span");
+    comma.textContent = ", ";
+    div.appendChild(positions[0]);
+    div.appendChild(comma);
+    div.appendChild(positions[1]);
+    div.appendChild(comma.cloneNode(true));
+    div.appendChild(positions[2]);
+    div.appendChild(document.createElement("br"));
+    */
+    table.appendChild(row);
+
 }
 
 var initObserver = true;
@@ -152,7 +361,7 @@ async function run() {
                     if (options[SHOW_BREEDS_STATS_OPTION]) {
                         dbg = anc;
                         let content = document.createElement("div");
-                        let stats = "H: " + axie.stats.hp + ", S: " + axie.stats.speed + ", M: " + axie.stats.morale;
+                        let stats = "H: " + axie.stats.hp + ", S: " + axie.stats.speed + ", M: " + axie.stats.morale + ", P: " + Math.round(axie.quality * 100) + "%";
                         if (axie.stage == 3) {
                             content.textContent = stats;
                             let extraDiv = document.createElement("div");
@@ -172,6 +381,36 @@ async function run() {
                         }
                         //prevent
                         if ((anc.firstElementChild.children[2].children[2].childElementCount == 1 && axie.stage != 3) || (axie.stage == 3 && anc.firstElementChild.children[2].children[2].childElementCount == 0)) {
+                            let traits = document.createElement("div");
+                            let table = document.createElement("table");
+                            appendTrait(table, {d: {name: "D"}, r1: {name: "R1"}, r2: {name: "R2"}});
+                            appendTrait(table, axie.traits.eyes);
+                            appendTrait(table, axie.traits.ears);
+                            appendTrait(table, axie.traits.mouth);
+                            appendTrait(table, axie.traits.horn);
+                            appendTrait(table, axie.traits.back);
+                            appendTrait(table, axie.traits.tail);
+                            traits.appendChild(table);
+                            traits.style.display = "none";
+                            traits.style.position = "absolute";
+                            traits.style["z-index"] = "9999";
+                            traits.style.background = "white";
+                            traits.style.border = "grey";
+                            traits.style["border-style"] = "solid";
+                            traits.style["border-width"] = "1px";
+                            traits.style["border-radius"] = "20px";
+                            traits.style["white-space"] = "nowrap";
+                            traits.style["padding-left"] = "10px"
+                            traits.style["padding-top"] = "10px";
+                            traits.style["padding-bottom"] = "10px";
+                            traits.style["padding-right"] = "10px";
+                            content.addEventListener("mouseover", function() {
+                                traits.style.display = "block";
+                            });
+                            content.addEventListener("mouseout", function() {
+                                traits.style.display = "none";
+                            });
+                            content.append(traits);
                             anc.firstElementChild.children[2].children[2].append(content);
                             //remove part's box margin to prevent overlap with price
                             anc.firstElementChild.children[3].style["margin-top"] = "0px";
